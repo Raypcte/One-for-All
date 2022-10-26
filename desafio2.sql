1ba4bc3b6c7b0ab1b21c50231b01c66d9f48a355
Raypcte <rayanevalak@gmail.com>
2022-10-26 14:22:00 -0300

    req2

--- desafio2.sql
+++ desafio2.sql
@@ -1,1 +1,8 @@
--- ADD  
+SELECT
+  COUNT(DISTINCT songs.song_id) as cancoes,
+  COUNT(DISTINCT albums.album_id) as albuns,
+  COUNT(DISTINCT artists.artist_id) as artistas
+
+FROM SpotifyClone.songs
+  INNER JOIN SpotifyClone.albums
+  INNER JOIN SpotifyClone.artists;
