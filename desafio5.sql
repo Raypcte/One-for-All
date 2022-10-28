ff441cedc786ab7a05708f9dbe2ad8e21f6c6c09
Raypcte <rayanevalak@gmail.com>
2022-10-28 13:25:59 -0300

    questao 5

--- desafio5.sql
+++ desafio5.sql
@@ -1,2 +1,10 @@
+SELECT songs.song_name AS 'cancao',
+  COUNT(history.song_id) AS 'reproducoes'
+  FROM SpotifyClone.songs AS songs
+INNER JOIN SpotifyClone.history history ON songs.song_id = history.song_id
+GROUP BY songs.song_name
+ORDER BY COUNT(history.song_id) DESC, songs.song_name ASC LIMIT 2;
+
+
 
 
