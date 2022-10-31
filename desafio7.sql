c2a92f1b9d11e0714658f95a1b2c3d31bab6885e
Raypcte <rayanevalak@gmail.com>
2022-10-31 09:51:04 -0300

    req8

--- desafio7.sql
+++ desafio7.sql
@@ -1,14 +1,3 @@
--- SELECT 
---   artists.artist_name AS 'artista',
---   albums.album_name AS 'album',
---   COUNT(following.user_id) AS 'seguidores'
-    
--- FROM SpotifyClone.albums 
---   INNER JOIN SpotifyClone.following AS fo ON al.artist_name = fo.artist_name
-
--- GROUP BY `artista`, `album`
--- ORDER BY `seguidores` DESC, `artista` ASC, `album` ASC;
-
 SELECT art.`artist_name` AS `artista`, alb.`name` AS `album`, COUNT(fol.`artist_id`) AS `seguidores`
 FROM SpotifyClone.`albums` AS alb
 
--- desafio8.sql
+++ desafio8.sql
@@ -1,21 +1,12 @@
-SELECT 
-  albums.name AS album,
-  artists.artist_name AS artista
-  
+SELECT
+  art.`artist_name` AS `artista`,
+  alb.`name` AS `album`
 
 FROM
-  SpotifyClone.artists AS artists
-  INNER JOIN SpotifyClone.albums AS albums
-  ON albums.artist_id = artists.artist_id
+  SpotifyClone.`artists` AS `art`
+INNER JOIN SpotifyClone.`albums` AS `alb`
+  ON art.`artist_id` = alb.`artist_id`
     
-WHERE artists.artist_name = 'Elis Regina'
+WHERE `artist_name` = 'Elis Regina'
 
-ORDER BY album;
-
-
--- SELECT art.artist_name AS artista, alb.name AS album
--- FROM SpotifyClone.artists AS art
--- INNER JOIN SpotifyClone.albums AS alb
--- ON art.artist_id = alb.artist_id
--- ORDER BY artista ASC;
-
+ORDER BY `album` ASC;
